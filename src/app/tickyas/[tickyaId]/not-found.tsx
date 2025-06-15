46c47880e03e5ba56a85963e9d48d1b58a85fffb
NRP <43085120+LotteFiber@users.noreply.github.com>
2025-06-15 16:08:51 +0700

    feaat: handle fallback routes
    * feat: add NotFound and Error components for improved error handling
    
    * remove unused path

---
+++ src/app/tickyas/[tickyaId]/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
+
+import { tickyasPath } from "@/paths";
+
+export default function NotFound() {
+  return (
+    <Placeholder
+      label="We could not find your records"
+      button={
+        <Button asChild variant="outline">
+          <Link href={tickyasPath()}>Go to Records page</Link>
+        </Button>
+      }
+    />
+  );
+}
--- src/app/tickyas/page.tsx
+++ src/app/tickyas/page.tsx
@@ -1,13 +1,18 @@
-import { Heading } from "@/components/heading";
+import { Suspense } from "react";
 
+import { Heading } from "@/components/heading";
+import { Spinner } from "@/components/spinner";
+        
 import { TickyaTable } from "@/features/tickya/components/tickya-table";
 
 const TickyasPage = () => {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Records Page" />
-      
-      <TickyaTable />
+
+      <Suspense fallback={<Spinner />}>
+        <TickyaTable />
+      </Suspense>
     </div>
   );
 };
