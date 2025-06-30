44efc6813374de401956d7e059e4af6130b90ff9
LotteFiber <ratchanonnine9@gmail.com>
2025-06-30 22:51:27 +0700

    hotfix: protect route on root path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { redirect } from "next/navigation";
 import { Heading } from "@/components/heading";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import { signInPath } from "@/paths";
 
 const HomePage = async () => {
+  const { user } = await getAuth();
+
+  if (!user) {
+    redirect(signInPath());
+  }
+
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Home Page" description="Your home place to start" />
