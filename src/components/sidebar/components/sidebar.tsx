31d032ffe200330618bb8506238f5694c20f6bdd
LotteFiber <ratchanonnine9@gmail.com>
2025-06-30 22:07:03 +0700

    feat: add authenticated sidebar

--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -2,10 +2,13 @@
 
 import { useState } from "react";
 import { cn } from "@/lib/utils";
+import { useAuth } from "@/features/auth/hooks/use-auth";
 import { navItems } from "../constants";
 import { SidebarItem } from "./sidebar-item";
 
 const Sidebar = () => {
+  const { user, isFetched } = useAuth();
+
   const [isTransition, setTransition] = useState(false);
   const [isOpen, setOpen] = useState(false);
 
@@ -15,9 +18,14 @@ const Sidebar = () => {
     setTimeout(() => setTransition(false), 200);
   };
 
+  if (!user || !isFetched) {
+    return null;
+  }
+
   return (
     <nav
       className={cn(
+        "animate-sidebar-from-left",
         "h-screen border-r pt-24",
         isTransition && "duration-200",
         isOpen ? "w-[78px] md:w-60" : "w-[78px]"
